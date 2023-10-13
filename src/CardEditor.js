77fde3133df60c71e43e5a6ad40159c4d26810ba
lemoncodes237 <111202168+lemoncodes237@users.noreply.github.com>
2023-10-13 17:14:06 -0400

    CardEditor made

---
+++ src/CardEditor.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './CardEditor.css'
+import React from 'react';
+
+class CardEditor extends React.Component {
+    constructor(props)  {
+        super(props);
+        this.state = { front: '', back: '' };
+        
+    }
+
+    addCard = () => {
+        this.props.addCard(this.state);
+        this.setState({front: '', back: ''});
+    };
+
+    deleteCard = index => this.props.deleteCard(index);
+
+    
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+        const cards = this.props.cards.map((card, index) =>  {
+            return (
+                <tr key={index}>
+                    <td>{card.front}</td>
+                    <td>{card.back}</td>
+                    <td>
+                        <button onClick={() => this.deleteCard(index)}>
+                            Delete Card
+                        </button>
+                    </td>
+                </tr>
+            );
+        });
+
+        return (
+            <div>
+                <h2>Card Editor</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Front</th>
+                            <th>Back</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cards}
+                    </tbody>
+                </table>
+                <br/>
+                <input 
+                name="front"
+                onChange={this.handleChange} placeholder="Front of card" value={this.state.front} />
+                
+                <input 
+                name="back"
+                onChange={this.handleChange} placeholder="Back of card" value={this.state.back} />
+                
+                <button onClick={this.addCard}>Add card</button>
+                <hr/>
+                <button onClick={this.props.switchMode}>Go to Card Viewer</button>
+            </div>
+        );
+    }
+}
+
+export default CardEditor;
